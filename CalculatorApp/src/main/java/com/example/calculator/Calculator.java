b507ed6e2abe66ba72156c25256486c2f15e3120
Kranthi-html <kranthi.akepogu@wissen.com>
2025-07-04 09:54:04 +0530

    logsadded

--- CalculatorApp/src/main/java/com/example/calculator/Calculator.java
+++ CalculatorApp/src/main/java/com/example/calculator/Calculator.java
@@ -1,17 +1,32 @@
 package com.example.calculator;
 
 public class Calculator {
+
     public int add(int a, int b) {
-        return a + b;
+        int result = a + b;
+        System.out.println("Adding " + a + " + " + b + " = " + result);
+        return result;
     }
+
     public int subtract(int a, int b) {
-        return a - b;
+        int result = a - b;
+        System.out.println("Subtracting " + a + " - " + b + " = " + result);
+        return result;
     }
+
     public int multiply(int a, int b) {
-        return a * b;
+        int result = a * b;
+        System.out.println("Multiplying " + a + " * " + b + " = " + result);
+        return result;
     }
+
     public int divide(int a, int b) {
-        if (b == 0) throw new IllegalArgumentException("Cannot divide by zero");
-        return a / b;
+        if (b == 0) {
+            System.out.println("Attempted to divide " + a + " by zero");
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        int result = a / b;
+        System.out.println("Dividing " + a + " / " + b + " = " + result);
+        return result;
     }
 }
